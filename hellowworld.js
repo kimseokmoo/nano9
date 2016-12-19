ae04d5a76c69a050cecb90f7d97900ac7b8c0c68
kimseokmoo <ksm9988@gmail.com>
2016-12-19 14:40:59 +0900

    add tell our world on github

--- hellowworld.js
+++ hellowworld.js
@@ -2,5 +2,5 @@ console.log('hello,world');
 console.log('Merry Christmas');
 console.log('Tell your world');
 console.log(Tell his world'):|;
-console.log('tell her world');
-
+console.log('tell her world'):
+console.log(tell our world'): 
